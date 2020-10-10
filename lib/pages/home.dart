64730a6676489eed6a26182969cd0ad9ac2eabc6
YertofeX <szabodaniel35@gmail.com>
2020-10-10 14:56:25 +0200

    home screen layout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,23 +10,109 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  final Color backgroundColor = Color(0xFF1D1E18);
+  final Color darkColor = Color(0xFF3F3F3F);
   final Color letterColor = Color(0xFFFEFEFD);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: backgroundColor,
-      body: Container(
-        child: SafeArea(
-            child: Scaffold(
-              appBar: AppBar(
-                title: Text('HOME'),
+      backgroundColor: Colors.grey[400],
+      body: SizedBox.expand(
+        child: Padding(
+          padding: EdgeInsets.only(top: 120.0, bottom: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Cím
+              Text(
+                'ciceró\nmondja',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 40.0,
+                  color: letterColor,
+                ),
               ),
-              body: Container(
-                child: Text('home.dart'),
+
+              // Gombok
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Játék indítása gomb
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Játék',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 16.0,
+                          color: letterColor,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: darkColor,
+                        minimumSize: Size(double.infinity, 60),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
+                      ),
+                    ),
+                    SizedBox(height: 30.0,),
+
+                    // Statisztikák gomb
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Statisztikák',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 16.0,
+                          color: letterColor,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: darkColor,
+                        minimumSize: Size(double.infinity, 60),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
+                      ),
+                    ),
+                    SizedBox(height: 30.0,),
+
+                    // Beállítások gomb
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Beállítások',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 16.0,
+                          color: letterColor,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: darkColor,
+                        minimumSize: Size(double.infinity, 60),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+
+              // Csapatnév
+              Text(
+                'Fejlesztették:\nPro-grammers',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 16.0,
+                  color: letterColor,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
