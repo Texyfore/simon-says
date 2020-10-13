64abaefcd62c3523b306f2ced76235ff432f0d3f
bela333 <b3kstudio@gmail.com>
2020-10-13 18:32:45 +0200

    Game appbar

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -7,13 +7,27 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
-
-  final Color backgroundColor = Color(0xFF1D1E18);
+  final Color backgroundColor = Color(0xFF3F3F3F);
   final Color letterColor = Color(0xFFFEFEFD);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 70.0,
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.pause_rounded,
+            size: 30.0,
+          ),
+          splashRadius: 25.0,
+        ),
+      ),
       backgroundColor: backgroundColor,
       body: Container(
         child: Text('game.dart'),
