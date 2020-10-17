2239ebd0264f2fa94c9273149ef62e5d632e0c15
YertofeX <szabodaniel35@gmail.com>
2020-10-17 18:22:16 +0200

    added GameOverWidget.dart

---
+++ lib/widgets/GameOverWidget.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PauseMenuWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: SizedBox.expand(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 200.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Szünet
+              Text(
+                'Játék Vége',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 50.0,
+                  color: Colors.redAccent,
+                  shadows: <Shadow>[
+                    letterShadow,
+                  ],
+                ),
+              ),
+              SizedBox(height: 40.0,),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.popUntil(context,  ModalRoute.withName('/home'));
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.clear_rounded,
+                      color: letterColor,
+                      size: 28.0,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'Kilépés',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 20.0,
+                        color: letterColor,
+                      ),
+                    ),
+                  ],
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: backgroundColor,
+                  minimumSize: Size(double.infinity, 60),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+final Color backgroundColor = Color(0xFF3F3F3F);
+final Color letterColor = Color(0xFFFEFEFD);
+final Shadow letterShadow = Shadow(
+  offset: Offset(1.0, 1.0),
+  blurRadius: 2,
+  color: Color.fromARGB(40, 0, 0, 0),
+);
